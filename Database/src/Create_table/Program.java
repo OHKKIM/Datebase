8dd6b74329b1c13afeb2fc18aa829027299303fb
OHKKIM <31759437+OHKKIM@users.noreply.github.com>
2019-11-27 01:44:31 +0900

    Create table Query

---
+++ Database/src/Create_table/Program.java
@@ -0,0 +1,30 @@
+package Create_table;
+import java.sql.*;
+
+public class Program
+{
+	public static void main(String args[])
+	{
+		Connection con = null;
+        Statement stmt = null;
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(
+			"jdbc:mysql://192.168.56.102:4567/Leisure_reserve","DBuser","1234");
+			stmt = con.createStatement();
+			
+			StringBuffer sql = new StringBuffer();
+			sql.append("create table Program");
+			sql.append("(P_name VARCHAR(40),"); 
+			sql.append("P_date DATE NOT NULL,");
+			sql.append("Address VARCHAR(40) NOT NULL,"); 
+			sql.append("Time TIME NOT NULL,"); 
+			sql.append("Price VARCHAR(40) NOT NULL,"); 
+			sql.append("Information TEXT,");
+			sql.append("PRIMARY KEY (P_name))");
+			
+			stmt.executeUpdate(sql.toString());
+			con.close();
+		}catch(Exception e){ System.out.println(e);}
+	}
+}
---
+++ Database/src/Create_table/User.java
@@ -0,0 +1,28 @@
+package Create_table;
+import java.sql.*;
+
+public class User
+{
+	public static void main(String args[])
+	{
+		Connection con = null;
+        Statement stmt = null;
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(
+			"jdbc:mysql://192.168.56.102:4567/Leisure_reserve","DBuser","1234");
+			stmt = con.createStatement();
+			
+			StringBuffer sql = new StringBuffer();
+			sql.append("create table User");
+			sql.append("(U_id INT AUTO_INCREMENT,"); 
+			sql.append("Pw VARCHAR(40) NOT NULL,");
+			sql.append("U_name VARCHAR(40) NOT NULL,"); 
+			sql.append("U_phone VARCHAR(40) NOT NULL,");
+			sql.append("PRIMARY KEY (U_id))");
+			
+			stmt.executeUpdate(sql.toString());
+			con.close();
+		}catch(Exception e){ System.out.println(e);}
+	}
+}
